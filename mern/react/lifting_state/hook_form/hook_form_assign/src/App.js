4338d6cdaec06ba051c65f691d771825b9aa44c7
Abel Woldemichael <abelllselam@gmail.com>
2023-11-11 20:44:28 -0500

    added validation

--- mern/react/lifting_state/hook_form/hook_form_assign/src/App.js
+++ mern/react/lifting_state/hook_form/hook_form_assign/src/App.js
@@ -3,10 +3,15 @@ import React, { useState } from "react";
 
 function App() {
   const [fName, setFName] = useState("");
+  const [fNameErr, setFNameErr] = useState("");
   const [lName, setLName] = useState("");
+  const [lNameErr, setLNameErr] = useState("");
   const [email, setEmail] = useState("");
+  const [emailErr, setEmailErr] = useState("");
   const [password, setPassword] = useState("");
+  const [passwordErr, setPasswordErr] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
+  const [confirmPasswordErr, setConfirmPasswordErr] = useState("");
 
   const createUser = (e) => {
     e.preventDefault();
@@ -22,6 +27,11 @@ function App() {
             value={fName}
             onChange={(e) => setFName(e.target.value)}
           />
+          <p>
+            {fName.length < 2 && fName.length > 0
+              ? "First Name field must be at least 2 characters."
+              : ""}
+          </p>
         </div>
         <div>
           <label>Last Name:</label>
@@ -30,6 +40,11 @@ function App() {
             value={lName}
             onChange={(e) => setLName(e.target.value)}
           />
+          <p>
+            {lName.length < 2 && lName.length > 0
+              ? "Last Name field must be at least 2 characters."
+              : ""}
+          </p>
         </div>
         <div>
           <label>Email:</label>
@@ -38,6 +53,11 @@ function App() {
             value={email}
             onChange={(e) => setEmail(e.target.value)}
           />
+          <p>
+            {email.length < 5 && email.length > 0
+              ? "Email field must be at least 5 characters."
+              : ""}
+          </p>
         </div>
         <div>
           <label>Password:</label>
@@ -46,6 +66,11 @@ function App() {
             value={password}
             onChange={(e) => setPassword(e.target.value)}
           />
+          <p>
+            {password.length < 8 && password.length > 0
+              ? "Password field must be at least 8 characters."
+              : ""}
+          </p>
         </div>
         <div>
           <label>Confirm Password:</label>
@@ -55,6 +80,11 @@ function App() {
             onChange={(e) => setConfirmPassword(e.target.value)}
           />
         </div>
+        <p>
+          {password !== confirmPassword && confirmPassword.length > 0
+            ? "Confirm Password field must match and at least be 8 characters."
+            : ""}
+        </p>
         {/* <button>Submit</button> */}
       </form>
 
