6454665a95f958842d4de3565ef2c47d0fb9d052
Abel Woldemichael <abelllselam@gmail.com>
2023-12-22 11:12:03 -0500

    new app

---
+++ java/springtoolsuit4/RentalCar/src/test/java/com/abel/rentalcar/RentalCarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abel.rentalcar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentalCarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
